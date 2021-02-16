d20e2b352bd09528687abc3c9925e9fa6595a9a5
Arnaud BIJU-DUVAL <abiju-du@e2r2p2.42.fr>
2021-02-16 14:39:31 +0100

    nouveau ft_split2.c

--- ft_split2.c
+++ ft_split2.c
@@ -6,7 +6,7 @@
 /*   By: abiju-du <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 10:11:28 by abiju-du          #+#    #+#             */
-/*   Updated: 2021/02/16 09:17:30 by abiju-du         ###   ########.fr       */
+/*   Updated: 2021/02/16 14:02:31 by abiju-du         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ int		next_cut(char const *s, char cut)
 
 	i = 0;
 
-	if (s[i] == cut)
+	if (i == cut)
 		i++;
 
 	while (s[i] != cut && s[i] != 0)
@@ -71,9 +71,11 @@ char	**filling_good(char const *s, char **tab, char c)
 
 	while (s[i])
 	{
-//printf("i = %d\n", i);
-//printf("j = %d\n", j);
-//printf("k = %d\n", k);
+printf("filling_good:\n");
+printf("i = %d\n", i);
+printf("j = %d\n", j);
+printf("k = %d\n", k);
+printf("s[i] : %c = %d\n", s[i], s[i]);
 		if (s[i] != c)
 		{
 			tab[j][k] = s[i];
@@ -82,7 +84,7 @@ char	**filling_good(char const *s, char **tab, char c)
 		}
 		else
 		{
-//printf("(else1)s[i] : %c\n", s[i]);	
+printf("(else1)s[i] : %c\n", s[i]);	
 //printf("k =: %d\n", k);	
 			tab[j][k] = 0;
 //printf("tab[j][k] : %c = %d\n", tab[j][k], tab[j][k]);	
@@ -110,67 +112,53 @@ char	**ft_split(char const *s, char c)
 	char	**tab = NULL;
 	int		i;
 	int		j;
-	int		nc;
+	int		pre;
 
-	i = 0;
-	j = 0;
-	nc = next_cut(s + i, c);
+	i	= 0;
+	j	= 0;
+	pre	= 0;
 
+printf("ft_split:\n");
+printf("MALLOC %d + 1\n\n\n", nb_words(s, c));
 
 	if (!(tab = malloc(sizeof(char*) * (nb_words(s, c) + 1))))
 		return (NULL);
 
-	while(s[i] == c)
-	{
-		if (!(tab[j] = malloc(sizeof(char))))
+printf("MALLOC %d + 1 sur j =  %d\n\n\n", next_cut(s + i, c), j);
+		if (!(tab[j] = malloc(sizeof(char) * next_cut(s + i + 1, c) + 1)))
+		{
+			freedom(tab, j);
 			return NULL;
-		i++;
-	}
-//printf("i = %d\nj = %d\nnext_cut = %d\n MALLOC\n\n\n", i, j, next_cut(&s[i], c));
-//	if (!(tab[j] = malloc(sizeof(char) * next_cut(&s[i], c)  + 1)))
-//	{
-/*		freedom(tab, j);*/
-//		return NULL;
-//	}
+		}
 
-	while (j <= nb_words(s, c))
+	while (s[i] != 0)
 	{
-printf("s[%d] : %c = %d\n", i, s[i], s[i]);
-printf("j = %d\n\n", j);
-		j++;
-		while (next_cut(s + i, c) != 1 && s[i] != 0)
-			i++;
-
-//printf("i = %d\n", i);
-
-		if (s[i + 1] == c)
+printf("				s[%d] = %c\n", i, s[i]);
+		if (s[i] == c)
 		{
 			i++;
-//printf("i2 = %d\n", i);
-printf("s[%d] : %c = %d\n", i, s[i], s[i]);
-printf("j = %d\n", j);
-printf("next_cut : %d\n MALLOC\n\n\n", next_cut(s + i, c));
-			if (!(tab[j] = malloc(sizeof(char) * next_cut(&s[i], c)  + 1)))
+			j++;
+printf("				_s[%d] = %c\n", i, s[i]);
+printf("MALLOC %d + 1 sur j = %d\n\n\n", next_cut(s +i, c), j);
+			if (!(tab[j] = malloc(sizeof(char) * next_cut(s + i, c) + 1)))
 			{
-/*				freedom(tab, j);*/
+//				freedom(tab, j);
 				return NULL;
 			}
-			i++;
 		}
-		if (s[i] == 0)
-			break;
+		i++;
 	}
 
-printf("C'est parti !!!\n\n");
 	tab = filling_good(s, tab, c);
 
-	return (tab);
+	return(tab);
+
 }
 
 #include <stdio.h>
 int		main()
 {
-	char	message[] = " Ceci n'est pas un message.";
+	char	message[] = "Ceci n'est pas un message.";
 	char cut = ' ';
 	int		i;
 
